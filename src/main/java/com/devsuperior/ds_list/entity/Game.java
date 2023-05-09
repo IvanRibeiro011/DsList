903da695771bb6d22d46fe949a129b39879ecc81
Ivanildo Ribeiro <ivanildo.ribeiro@arphoenix.com.br>
2023-05-09 10:56:00 -0300

    properties, Game entity, ORM

---
+++ src/main/java/com/devsuperior/ds_list/entity/Game.java
@@ -0,0 +1,41 @@
+package com.devsuperior.ds_list.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.Hibernate;
+
+import java.util.Objects;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
+@Entity(name = "tb_game")
+public class Game {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    @Column(name = "game_year")
+    private Integer year;
+    private String genre;
+    private String platforms;
+    private Double score;
+    private String imgUrl;
+    private String shortDescription;
+    private String longDescription;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || Hibernate.getClass(this) != Hibernate.getClass(o)) return false;
+        Game game = (Game) o;
+        return getId() != null && Objects.equals(getId(), game.getId());
+    }
+
+    @Override
+    public int hashCode() {
+        return getClass().hashCode();
+    }
+}
