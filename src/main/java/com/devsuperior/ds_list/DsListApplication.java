5cd6c44b3e5980c60a02b3b5ccc4fe225a4a0365
Ivanildo Ribeiro <ivanildo.ribeiro@arphoenix.com.br>
2023-05-09 10:26:39 -0300

    Project Created

---
+++ src/main/java/com/devsuperior/ds_list/DsListApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.ds_list;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsListApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsListApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devsuperior/ds_list/DsListApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.ds_list;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
